3ed6a6eedce8e2c4566c792353c531d41ada02d6
Aleksandr Tolkanev <allcars61@gmail.com>
2023-05-27 22:44:04 +0300

    Update test_courses_api.py

--- tests/students/test_courses_api.py
+++ tests/students/test_courses_api.py
@@ -26,6 +26,55 @@ def student_factory():
     return factory
 
 
+import pytest
+
+
+@pytest.mark.django_db
+def test_retrieve_first_course(api_client, course_factory):
+    # Создаем курс с помощью фабрики
+    course = course_factory()
+
+    # Выполняем GET-запрос для получения первого курса
+    response = api_client.get(f"/api/v1/courses/{course.id}/")
+
+    # Проверяем, что вернулся статус-код 200
+    assert response.status_code == 200
+
+    # Проверяем, что вернулся ожидаемый JSON-объект
+    assert response.json() == {
+        "id": course.id,
+        "name": course.name,
+        "students": [],
+    }
+
+
+@pytest.mark.django_db
+def test_list_courses(api_client, course_factory):
+    # Создаем несколько курсов с помощью фабрики
+    course1 = course_factory()
+    course2 = course_factory()
+
+    # Выполняем GET-запрос для получения списка курсов
+    response = api_client.get("/api/v1/courses/")
+
+    # Проверяем, что вернулся статус-код 200
+    assert response.status_code == 200
+
+    # Проверяем, что вернулся список всех курсов
+    assert response.json() == [
+        {
+            "id": course1.id,
+            "name": course1.name,
+            "students": [],
+        },
+        {
+            "id": course2.id,
+            "name": course2.name,
+            "students": [],
+        },
+    ]
+
+
 @pytest.mark.django_db
 def test_filter_courses_by_id(api_client, course_factory):
     # Создаем 2 курса с разными id
